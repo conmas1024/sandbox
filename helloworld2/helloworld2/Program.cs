ef4ef6e734d840e0a50675a89a23290f4c1d90e4
conmas1024 <conmas.biblous@gmail.com>
2014-06-06 00:22:49 +0900

    test commit

--- helloworld2/helloworld2/Program.cs
+++ helloworld2/helloworld2/Program.cs
@@ -10,7 +10,7 @@ namespace helloworld2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World 2 !");
+            Console.WriteLine("Hello World 22222 !");
         }
     }
 }
